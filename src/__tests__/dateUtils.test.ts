c9583345f9930db74e3c62f4ad9ad1d6bb2810c1
leifurb <leifurbenedikt@gmail.com>
2023-01-26 12:36:42 +0000

    test for isWithinRange func

--- src/__tests__/dateUtils.test.ts
+++ src/__tests__/dateUtils.test.ts
@@ -12,27 +12,34 @@ describe("Gets the current year", () => {
 
 describe("Adds to x number of date unit types to date", () => {
   it("Should add x number of days to date", () => {
-    const results = add(moment("2022/12/01").toDate(), 10);
+    const results = add(moment("2022/12/01", "YYYY/MM/DD").toDate(), 10);
     expect(moment(results).format("YYYY/MM/DD")).toBe("2022/12/11");
   });
   it("Should add x number of years to date", () => {
-    const results = add(moment("2022/12/01").toDate(), 10, DATE_UNIT_TYPES.YEARS);
+    const results = add(moment("2022/12/01", "YYYY/MM/DD").toDate(), 10, DATE_UNIT_TYPES.YEARS);
     expect(moment(results).format("YYYY/MM/DD")).toBe("2032/12/01");
   });
   it("Should add x number of minutes to date", () => {
-    const results = add(moment("2022/12/01").toDate(), 10, DATE_UNIT_TYPES.MINUTES);
+    const results = add(moment("2022/12/01", "YYYY/MM/DD").toDate(), 10, DATE_UNIT_TYPES.MINUTES);
     expect((results).getMinutes()).toBe(10);
   });
   it("Should add x number of seconds to date", () => {
-    const results = add(moment("2022/12/01").toDate(), 20, DATE_UNIT_TYPES.SECONDS);
+    const results = add(moment("2022/12/01", "YYYY/MM/DD").toDate(), 20, DATE_UNIT_TYPES.SECONDS);
     expect((results).getSeconds()).toBe(20);
   });
   it("Should add x number of weeks to date", () => {
-    const results = add(moment("2022/01/01").toDate(), 4, DATE_UNIT_TYPES.WEEKS);
+    const results = add(moment("2022/01/01", "YYYY/MM/DD").toDate(), 4, DATE_UNIT_TYPES.WEEKS);
     expect(moment(results).format("YYYY/MM/DD")).toBe("2022/01/29");
   });
   it("Should add x number of months to date", () => {
-    const results = add(moment("2022/01/01").toDate(), 4, DATE_UNIT_TYPES.MONTHS);
+    const results = add(moment("2022/01/01", "YYYY/MM/DD").toDate(), 4, DATE_UNIT_TYPES.MONTHS);
     expect(moment(results).format("YYYY/MM/DD")).toBe("2022/05/01");
   });
 });
+describe("Checkes if date is within 2 dates", () => {
+  it("Should return true if date is between 2 dates", () => {
+    const results = isWithinRange(moment("2015/01/01", "YYYY/MM/DD").toDate(), moment("2010/01/01", "YYYY/MM/DD").toDate(), moment("2020/01/01", "YYYY/MM/DD").toDate());
+    expect(results).toBe(true);
+  });
+});
+
