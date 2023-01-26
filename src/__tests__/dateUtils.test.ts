05ae2e5b59ba5dace8d8cea6f2a0a5b706eb7bd6
leifurb <leifurbenedikt@gmail.com>
2023-01-26 12:30:23 +0000

    adding additional tests for add function

--- src/__tests__/dateUtils.test.ts
+++ src/__tests__/dateUtils.test.ts
@@ -1,6 +1,7 @@
 import {describe, expect, it} from 'vitest';
 import { getCurrentYear, add, isWithinRange } from '../dateUtils';
 import moment from 'moment';
+import { DATE_UNIT_TYPES } from '../constants';
 
 describe("Gets the current year", () => {
   it("Should fetch the current year", () => {
@@ -9,9 +10,29 @@ describe("Gets the current year", () => {
   });
 });
 
-describe("Adds to x number of days to date", () => {
+describe("Adds to x number of date unit types to date", () => {
   it("Should add x number of days to date", () => {
     const results = add(moment("2022/12/01").toDate(), 10);
     expect(moment(results).format("YYYY/MM/DD")).toBe("2022/12/11");
+  });
+  it("Should add x number of years to date", () => {
+    const results = add(moment("2022/12/01").toDate(), 10, DATE_UNIT_TYPES.YEARS);
+    expect(moment(results).format("YYYY/MM/DD")).toBe("2032/12/01");
+  });
+  it("Should add x number of minutes to date", () => {
+    const results = add(moment("2022/12/01").toDate(), 10, DATE_UNIT_TYPES.MINUTES);
+    expect((results).getMinutes()).toBe(10);
+  });
+  it("Should add x number of seconds to date", () => {
+    const results = add(moment("2022/12/01").toDate(), 20, DATE_UNIT_TYPES.SECONDS);
+    expect((results).getSeconds()).toBe(20);
+  });
+  it("Should add x number of weeks to date", () => {
+    const results = add(moment("2022/01/01").toDate(), 4, DATE_UNIT_TYPES.WEEKS);
+    expect(moment(results).format("YYYY/MM/DD")).toBe("2022/01/29");
+  });
+  it("Should add x number of months to date", () => {
+    const results = add(moment("2022/01/01").toDate(), 4, DATE_UNIT_TYPES.MONTHS);
+    expect(moment(results).format("YYYY/MM/DD")).toBe("2022/05/01");
   });
 });
