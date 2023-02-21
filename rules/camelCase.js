e32c2eebeed7c3952d51f1965dbf4e176c59eb53
leifurb <leifurbenedikt@gmail.com>
2023-02-21 21:56:16 +0000

    adding back in right code for camel case node_module fuck up

--- rules/camelCase.js
+++ rules/camelCase.js
@@ -2,19 +2,17 @@ module.exports = {
   create(context) {
     return {
       FunctionDeclaration(node) {
-        if (
-          node.id.name !== undefined &&
-          !/^[a-z]+([A-Z][a-z0-9]+)*$/.test(node.id.name)
-        ) {
+        if (node.id.name !== undefined && !(/^[a-z]+([A-Z][a-z0-9]+)*$/.test(node.id.name))) {
           context.report({
             node: node.id,
             message: "Function name '{{name}}' should be in camelCase.",
             data: {
-              name: node.id.name,
-            },
+              name: node.id.name
+            }
           });
         }
-      },
+      }
     };
-  },
+}
 };
+
